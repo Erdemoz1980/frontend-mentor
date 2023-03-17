830011a11f9071386584b316bf64460dc47b8c3b
Erdem Ozdemir <erdemozproductions@gmail.com>
2023-03-17 01:56:51 -0400

    script iniital + minor style changes

---
+++ interactive-card-details-form-main/script.js
@@ -0,0 +1,13 @@
+const submitForm = document.querySelector('#submit-form');
+const submitBtn = document.querySelector('.btn');
+const nameInput = document.getElementById('name');
+const numberInput = document.getElementById('number');
+const monthInput = document.getElementById('month');
+const yearInput = document.getElementById('year');
+const cvcIntput = document.getElementById('cvc');
+
+submitForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+})
+
