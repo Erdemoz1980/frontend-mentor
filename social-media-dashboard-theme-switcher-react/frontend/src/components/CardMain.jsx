182aec99f1b678e55bfd48a4b8f1098e5c9b03fb
Erdem Ozdemir <erdemozproductions@gmail.com>
2023-03-24 15:26:11 -0400

    border issue solved

--- social-media-dashboard-theme-switcher-react/frontend/src/components/CardMain.jsx
+++ social-media-dashboard-theme-switcher-react/frontend/src/components/CardMain.jsx
@@ -2,19 +2,23 @@
 const CardMain = ({ profile, icons:{upIcon, downIcon} }) => {
   const { name, platform, followers, icon } = profile;
 
-  const borderTop = {
-    borderTop:`4px solid var(--clr${platform})`
-}
-
   const folChg = followers.current - followers.previous;
   const absFolChg = Math.abs(folChg);
   
   const folChgColor = {
   color:folChg > 0 ? 'var(--clrLimeGreen)' : 'var(--clrBrightRed)'
-}
+  }
+
+  const border = {
+    borderTop: `4px solid ${platform === 'Instagram' ? '' : `var(--clr${platform})`}`,
+    borderImage: platform === 'Instagram' ? `var(--clr${platform})` : '',
+    borderImageSlice:platform === 'Instagram' ? '1' : '',
+  }
   
+ 
   return (
-    <div className="card" style={borderTop}>
+    <div className="card-container">
+    <div className="card-main" style={border}>
       <div className="card-header">
         <img src={icon} alt="icon" />
         <p>{name}</p>
@@ -27,7 +31,8 @@ const CardMain = ({ profile, icons:{upIcon, downIcon} }) => {
         <img src={folChg > 0 ? upIcon : downIcon} alt='icon' /> {absFolChg} Today
       </footer>
 
-    </div>
+      </div>
+      </div>
   )
 }
 
