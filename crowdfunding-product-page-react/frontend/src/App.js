47c90c6cf71713959ffd32fd50e716608a7a8608
Erdem Ozdemir <erdemozproductions@gmail.com>
2023-03-28 12:58:21 -0400

    intialized database

---
+++ crowdfunding-product-page-react/frontend/src/App.js
@@ -0,0 +1,16 @@
+
+
+const App = () => {
+  return (
+    <>
+      <div className='container'>
+        
+      <h1>Crowdfunding Page</h1>
+     </div>
+    
+    </>
+      
+  )
+}
+
+export default App
