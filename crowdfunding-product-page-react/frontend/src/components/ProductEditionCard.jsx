e45ae796e61356805c35111d2fbdc3fbf35ef1a8
Erdem Ozdemir <erdemozproductions@gmail.com>
2023-03-29 16:27:38 -0400

    desktop component+styling complete

--- crowdfunding-product-page-react/frontend/src/components/ProductEditionCard.jsx
+++ crowdfunding-product-page-react/frontend/src/components/ProductEditionCard.jsx
@@ -1,18 +1,25 @@
 
 const ProductEditionCard = ({ edition }) => {
   const { name, desc, min_pledge, countInStock } = edition;
+  
+  const opacity = {
+    opacity:countInStock > 0 ? '1' :'0.5'
+  }
+  const btnStyle = {
+    background:countInStock > 0 ? 'var(--clrModerateCyan)' : 'var(--clrDarkGray)'
+  }
   return (
-    <div className="edition-card">
+    <div className="edition-card" style={opacity}>
       <header className="edition-header">
         <h2>{name}</h2>
         <p>Pledge ${min_pledge} or more</p>
       </header>
-      <div className="edition-text">
-        <p>{desc}</p>
-      </div>
+        <p className="edition-text">{desc}</p>
       <footer className="edition-footer">
-        <div><h1>{countInStock}</h1>left</div>
-        <button className="btn btn-edition">Select Reward</button>
+        <div><h1>{countInStock}</h1>
+          <p>left</p>
+        </div>
+        <button className="btn btn-edition" style={btnStyle}>{countInStock>0?'Select Reward' : 'Out of stock'}</button>
       </footer>
     </div>
   )
