9f20101b2adecb368d2b6890c8d4bde14b57d093
Erdem Ozdemir <erdemozproductions@gmail.com>
2023-04-07 17:09:50 -0400

    css specificity issues+dry code

--- tip-calculator-app-react/frontend/src/components/BillSplitterPanel.jsx
+++ tip-calculator-app-react/frontend/src/components/BillSplitterPanel.jsx
@@ -2,32 +2,31 @@
 const BillSplitterPanel = () => {
   return (
     <div className="panel splitter-panel">
-     
+
       <div className="bill-input-group">
         <p>Bill</p>
-        <label htmlFor="bill-input" className="label-util">
-        <input type="text" name="bill" id="bill" className="bill-input" placeholder="0" />
+        <label htmlFor="bill-input">
+          <input type="text" name="bill" id="bill-input"
+            className="input-field bill" placeholder="0" />
       </label>
-     
-
       </div>
-   
+
       <div className="tip-calculator">
         <p className="tip-title">Select Tip %</p>
-        <section className="tip-percentage-container">
-          <div className="percentage">5%</div>
-          <div className="percentage">10%</div>
-          <div className="percentage">15%</div>
-          <div className="percentage">25%</div>
-          <div className="percentage">50%</div>
-          <div className="percentage custom label-util">Custom</div>
+        <section className="percentage-grid">
+          <button className="percentage">5%</button>
+          <button className="percentage">10%</button>
+          <button className="percentage">15%</button>
+          <button className="percentage">25%</button>
+          <button className="percentage">50%</button>
+          <input className="percentage custom" type="text" name="" id="custom" placeholder="Custom"/>
         </section>
       </div>
       
       <div className="nbr-input-group">
         <p>Number of People</p>
-        <label htmlFor="nbr-people" className="label-util">
-        <input type="text" name='nbr-people' id='nbr-people' className="nbr-people-input" placeholder="0"/>
+        <label htmlFor="nbr-people">
+        <input type="text" name='nbr-people' id='nbr-people' className="input-field nbr-people" placeholder="0"/>
       </label>
       </div>
     </div>
