8e0daaa8aebb03a4aed627ef0f7dd00e58ccc54a
Erdem Ozdemir <erdemozproductions@gmail.com>
2023-04-13 03:14:35 -0400

    pricing panel logic

--- interactive-pricing-component-react/frontend/src/App.js
+++ interactive-pricing-component-react/frontend/src/App.js
@@ -1,14 +1,14 @@
 import Header from './components/Header';
 import PricingPanel from "./components/PricingPanel"
 import BenefitsPanel from './components/BenefitsPanel';
-import IconCheck from './components/IconCheck';
+import data from './data.json'
 
 const App = () => {
   return (
     <>
       <Header />
       <div className='panel-container'>
-       <PricingPanel />
+       <PricingPanel data={data} />
        <BenefitsPanel />
       </div>
    
--- interactive-pricing-component-react/frontend/src/components/PricingPanel.jsx
+++ interactive-pricing-component-react/frontend/src/components/PricingPanel.jsx
@@ -1,30 +1,46 @@
 import { useState } from 'react';
 
-const PricingPanel = () => {
-  const [sliderValue, setSliderValue] = useState(5);
+const PricingPanel = ({data}) => {
+  const [sliderValue, setSliderValue] = useState(0);
+  const [checked, setChecked] = useState(false);
+  const [planValue, setPlanValue] = useState(Number(data[0].price))
   
   const handleSliderChange = (e) => {
-    setSliderValue(e.target.value);
+    const value = e.target.value;
+    setSliderValue(value);
+    setPlanValue(Number(data[value].price));
   }
 
   const sliderTrackStyle = {
-    '--sliderTrackWidth': `${(sliderValue / 5) * 100}%`
+    '--sliderTrackWidth': `${(sliderValue / 4) * 100}%`
+  };
+
+  const radioCheckHandler = () => {
+    setChecked(!checked);
+    /*Set the discount*/ 
+    setPlanValue(prevPlanValue => {
+      return checked ? prevPlanValue / 0.75 : prevPlanValue * 0.75;
+    });
   };
 
   return (
     <div className="pricing-panel">
       <section className="pricing-panel-header">
         <h4 className="views-count text">100K Pageviews</h4>
-        <h1 className="price">$16.00 <span className='text'>/month</span></h1>
+        <h1 className="price">${Number(planValue).toFixed(2)}<span className='text'>/month</span></h1>
       </section>
 
       <label htmlFor="price-slider">
-        <input className='price-slider' type="range" name="price-slider" id="price-slider" min="0" max="5" value={sliderValue} onChange={handleSliderChange} style={sliderTrackStyle} />
+        <input className='price-slider' type="range" name="price-slider" id="price-slider" min="0" max="4" value={sliderValue} onChange={handleSliderChange} style={sliderTrackStyle} />
      </label>
       <section className="billing-container">
         <p className='billing-title text'>Monthly Billing</p>
-        <label htmlFor="billing-cycle">
-           <input type="radio" name="billing" id="billing-cycle" />
+        <label className='billing-cycle-label' htmlFor="billing-cycle">
+          <input type="radio" name="billing" id="billing-cycle"
+            checked={checked}
+            onClick={radioCheckHandler}
+          />
+         <span></span>
         </label>
         <p className='billing-title text'>Yearly Billing</p>
         <p className='billing-title small'>25% discount</p>
