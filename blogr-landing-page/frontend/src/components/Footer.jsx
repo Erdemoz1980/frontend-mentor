450d1ecc6a9f6033fd86ba51d12852e003169669
Erdem Ozdemir <erdemozproductions@gmail.com>
2023-05-10 17:32:33 -0400

    bloggr desktop styling complete

--- blogr-landing-page/frontend/src/components/Footer.jsx
+++ blogr-landing-page/frontend/src/components/Footer.jsx
@@ -4,35 +4,34 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="container">
-        <img src={logo} alt='company logo' className='logo' />
-       
+        <div className="footer-wrapper">
+            <img src={logo} alt='company logo' className='logo' />
         <nav className="footer-nav">
-          <ul>
-            <li><a href="/">Product</a></li>
-            <li><a href="/">Overview</a></li>
-            <li><a href="/">Pricing</a></li>
-            <li><a href="/">Marketplace</a></li>
-            <li><a href="/">Integrations</a></li>
-          </ul>
-          <ul>
-            <li><a href="/">Company</a></li>
-            <li><a href="/">About</a></li>
-            <li><a href="/">Team</a></li>
-            <li><a href="/">Blog</a></li>
-            <li><a href="/">Careers</a></li>
-          </ul>
+            <ul>
+              <li><a href="/">Product</a></li>
+              <li><a href="/">Overview</a></li>
+              <li><a href="/">Pricing</a></li>
+              <li><a href="/">Marketplace</a></li>
+              <li><a href="/">Integrations</a></li>
+            </ul>
+            <ul>
+              <li><a href="/">Company</a></li>
+              <li><a href="/">About</a></li>
+              <li><a href="/">Team</a></li>
+              <li><a href="/">Blog</a></li>
+              <li><a href="/">Careers</a></li>
+            </ul>
           <ul>
             <li><a href="/">Connect</a></li>
             <li><a href="/">Contact</a></li>
             <li><a href="/">Newsletter</a></li>
             <li><a href="/">LinkedIn</a></li>
-          </ul>
-        </nav>
+            </ul>
+          </nav>
+        </div>
       </div>
-
-
     </footer>
   )
-}
+};
 
 export default Footer
