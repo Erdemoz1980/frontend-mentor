f6a306c5ddc4d44ee7733a1b305b9b2b89a340c0
Erdem Ozdemir <erdemozproductions@gmail.com>
2023-05-30 20:03:35 -0400

    details styling and next/prev logic

--- minimalist-portfolio-website/frontend/src/components/PortfolioDetails.jsx
+++ minimalist-portfolio-website/frontend/src/components/PortfolioDetails.jsx
@@ -1,51 +1,83 @@
 import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import portfolioData from '../portfolioData.json';
+import arrowRight from '../images/icons/arrow-right.svg';
+import arrowLeft from '../images/icons/arrow-left.svg';
 
 const PortfolioDetails = () => {
   const { id } = useParams()
 
-  const portfolioItem = portfolioData.find(item => item.id.toString() === id);
-  const { title, description } = portfolioItem;
+  const currentItem = portfolioData.find(item => item.id.toString() === id);
+  const { title, description } = currentItem;
 
-  const { imgDeskHero, projectBackground, keywords, techStack, staticPreviews } = portfolioItem.details;
-  
+  const { imgDeskHero, projectBackground, keywords, techStack, staticPreviews } = currentItem.details;
+
+  const prevItem = portfolioData.indexOf(currentItem) === 0 ? portfolioData[portfolioData.length - 1] : portfolioData[portfolioData.indexOf(currentItem) - 1];
+  const nextItem = portfolioData.indexOf(currentItem) === portfolioData.length - 1 ? portfolioData[0] : portfolioData[portfolioData.indexOf(currentItem) + 1];
+
   return (
     <div className="container">
-        <div className="details-hero">
-          <img src={imgDeskHero} alt="hero display" />
-        </div>
-        <div className='portfolio-details-wrapper'>
-        <div className="details-description">
+      <div className="details-hero">
+        <img src={imgDeskHero} alt="hero display" />
+      </div>
+      <div className='portfolio-details-wrapper'>
+        <div className="details-description-wrapper">
           <h2>{title}</h2>
-          <p>{description}</p>
-          <p>
-            {keywords.map(keyword => (
-              <span key={keyword.id}>{keyword.name} </span>
+          <p className='description'>{description}</p>
+          <p className='keywords-wrapper'>
+            {keywords.map((keyword, index) => (
+              <span key={keyword.id} className='keywords'>
+                {keyword.name}
+                {index !== keywords.length - 1 && ' / '}
+              </span>
             ))}
           </p>
-          <p>
-            {techStack.map(tech => (
-              <span key={tech.id}>{tech.name}</span>
+          <p className='techstack-wrapper'>
+            {techStack.map((tech, index) => (
+              <span key={tech.id} className='keywords'>
+                {tech.name}
+                {index !== techStack.length - 1 && ' / '}
+              </span>
             ))}
           </p>
-          <div className="btn btn-secondary">Visit Website</div>
+          <button className="btn btn-secondary btn-md">Visit Website</button>
         </div>
-
         <div className="project-background">
           <h3>Project Background</h3>
-          <p>{projectBackground}</p>
-        </div>
+          <p className='project-background-text'>{projectBackground}</p>
 
-        <div className="static-previews-wrapper">
-          {staticPreviews.map(preview => (
-            <div key={preview.id} className='preview-image-wrapper'>
-              <img src={preview.image} alt="" />
-            </div>
-          ))}
+          <div className="static-previews-wrapper">
+            <h3>Static Previews</h3>
+            {staticPreviews.map(preview => (
+              <div key={preview.id} className='static-preview-img'>
+                <img src={preview.image} alt="preview" />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
+      <nav className="details-nav">
+        <div className="prev-details">
+          <Link to={`/portfolio/${prevItem.id}`}>
+            <img src={arrowLeft} alt='arrow left' />
+          </Link>
+          <div className="prev-navigation">
+            <h3>{prevItem.title}</h3>
+            <p>Previous Project</p>
+          </div>
+        </div>
+        <div className="next-details">
+          <div className="next-navigation">
+            <h3>{nextItem.title}</h3>
+            <p>Next Project</p>
+          </div>
+          <Link to={`/portfolio/${nextItem.id}`}>
+            <img src={arrowRight} alt='arrow right' />
+          </Link>
+        </div>
+      </nav>
     </div>
   )
-}
+};
 
 export default PortfolioDetails
