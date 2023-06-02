f977b4c4369c80f55b0be2a1a1e135cefad0afd1
Erdem Ozdemir <erdemozproductions@gmail.com>
2023-06-02 02:07:40 -0400

    form validation logic

--- minimalist-portfolio-website/frontend/src/components/ContactForm.jsx
+++ minimalist-portfolio-website/frontend/src/components/ContactForm.jsx
@@ -1,11 +1,28 @@
+import { useState } from 'react';
 
 const ContactForm = ({ formData, setFormData }) => {
+  const [alert, setAlert] = useState({
+    name:false,
+    email: false,
+    message:false
+  });
   const { name, email, message } = formData
    
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log(formData)
-  }
+
+    const updatedAlert = {
+      name: name === '',
+      email: email === '',
+      message: message === ''
+    }
+    setAlert({ ...alert, ...updatedAlert });
+    if (Object.values(updatedAlert).every(field => field === false)) {
+      console.log(formData)
+    }
+  };
+
+  
 
   return (
     <div className="container">
@@ -15,15 +32,27 @@ const ContactForm = ({ formData, setFormData }) => {
         <form className="contact-form" onSubmit={submitHandler}>
           <div className="form-group">
             <label htmlFor="name">Name</label>
-            <input type="text" name="name" id="name" value={name} placeholder="Please enter your name" onChange={(e) => setFormData(prevState => ({ ...prevState, name: e.target.value }))} />
+            <input type="text" name="name" id="name" className={`${alert.name ? 'alert' : ''}`} value={name} placeholder="Please enter your name"
+              onChange={(e) => {
+                setFormData(prevState => ({ ...prevState, name: e.target.value }));
+                setAlert(prevState=>({...prevState,name:false}))
+              }} />
           </div>
           <div className="form-group">
             <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="email" value={email} placeholder='Please enter your email address' onChange={(e) => setFormData(prevState => ({ ...prevState, email: e.target.value }))} />
+            <input type="email" name="email" id="email" className={`${alert.email ? 'alert' : ''}`} value={email} placeholder='Please enter your email address'
+              onChange={(e) => {
+                setFormData(prevState => ({ ...prevState, email: e.target.value }));
+                setAlert(prevState=>({...prevState, email:false}))
+              }}/>
           </div>
           <div className="form-group">
             <label htmlFor="message">Message</label>
-            <textarea name="message" id="message" value={message} placeholder="How can I help?" onChange={(e) => setFormData(prevState => ({ ...prevState, message: e.target.value }))} ></textarea>
+            <textarea name="message" id="message" className={`${alert.message ? 'alert' : ''}`} value={message} placeholder="How can I help?"
+              onChange={(e) => {
+                setFormData(prevState => ({ ...prevState, message: e.target.value }));
+                setAlert(prevState=>({...prevState, message:false}))
+              }} ></textarea>
           </div>
        
           <button type="submit" className="btn btn-main">Send Message</button>
