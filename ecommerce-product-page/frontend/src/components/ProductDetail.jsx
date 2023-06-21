805a9dd2c7ba728d7bfd9f3bc9f13c8fd35a6a50
Erdem Ozdemir <erdemozproductions@gmail.com>
2023-06-21 14:50:47 -0400

    product detail component complete

--- ecommerce-product-page/frontend/src/components/ProductDetail.jsx
+++ ecommerce-product-page/frontend/src/components/ProductDetail.jsx
@@ -1,61 +1,58 @@
 import { useState } from 'react';
-import iconMinus from '../images/icon-minus.svg';
-import iconPlus from '../images/icon-plus.svg';
-import iconCart from '../images/icon-cart.svg';
+import IconCart from './IconCart';
+import IconMinus from './IconMinus';
+import IconPlus from './IconPlus';
 
-const ProductDetail = ({company, name, description, price, discount, imageThumbnails, imagesMain }) => {
+const ProductDetail = ({ company, name, description, price, discount, imageThumbnails, imagesMain }) => {
   const [activeImage, setActiveImage] = useState(0);
   const [qty, setQty] = useState(0);
 
+  price = price.toFixed(2)
+
   function addToCartHandler() {
-    
   }
 
   return (
     <div className="product-page-wrapper">
-
       <div className="product-display-wrapper">
         <div className="product-main-image-wrapper">
-          <img src={imagesMain[activeImage]} alt="main product"/>
+          <img src={imagesMain[activeImage]} alt="main product" />
         </div>
         <div className="product-thumbnails">
           {
             imageThumbnails.map((thumbnail, index) => (
-              <div key={index} className={`thumbnail-wrapper ${activeImage===index ? 'active' : ''}`} onClick={()=>setActiveImage(index)}>
-                <img src={thumbnail} alt="thumbnail"/>
+              <div key={index} className={`thumbnail-wrapper ${activeImage === index ? 'active' : ''}`} onClick={() => setActiveImage(index)}>
+                <img src={thumbnail} alt="thumbnail" />
               </div>
             ))
           }
         </div>
       </div>
-
       <div className="product-info-wrapper">
         <h4 className='company-name'>{company}</h4>
-        <h1 className='product-name'>{name}</h1>
+        <h1 className='product-name text-dark'>{name}</h1>
         <p className="product-description">{description}</p>
         <div className="price-wrapper">
-          <h2 className='product-price'>${`${discount ? price-(price*(discount/100)) : price}`}</h2>{discount && <span className='discount-tab'>{discount}%</span>}
-          {discount && <small className='original-price'>${price}</small>}
+          <h2 className='product-price text-dark'>${`${discount ? (price - (price * (discount / 100))).toFixed(2) : price}`}{discount && <span className='discount-tab'>{discount}%</span>}</h2>
+          {discount && <small className='original-price text-light'>${price}</small>}
         </div>
-        <div className="add-to-cart-wrapper">
-          <div className="quantity-wrapper">
-            <button className='btn btn-qty' disabled={qty===0} onClick={()=>setQty(prevState=>prevState-1)}>
-              <img src={iconMinus} alt="decrease" />
+        <div className="cart-control-wrapper">
+          <div className="qty-wrapper">
+            <button className='btn btn-qty' disabled={qty === 0} onClick={() => setQty(prevState => prevState - 1)}>
+              <IconMinus disabled={qty===0} />
             </button>
-            <p className='qty'>{qty}</p>
-            <button className='btn btn-qty' onClick={()=>setQty(prevState=>prevState+1)}>
-              <img src={iconPlus} alt="increase" />
+            <p className='qty-info text-dark'>{qty}</p>
+            <button className='btn btn-qty' onClick={() => setQty(prevState => prevState + 1)}>
+             <IconPlus />
             </button>
           </div>
-          <button className="btn btn-main" onClick={addToCartHandler}>
-            <img src={iconCart} alt='cart'/>
+          <button className="btn btn-primary" onClick={addToCartHandler}>
+            <IconCart type='primary' />
             Add to cart</button>
         </div>
-        
       </div>
-
     </div>
   )
-}
+};
 
 export default ProductDetail
